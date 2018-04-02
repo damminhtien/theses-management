da9ae59eaaa2072604d5663bb52668afd38a73a5
damminhtien <damminhtienchl@gmail.com>
2018-04-02 21:34:55 +0700

    modify theme admin

--- src/routes/khoavien.js
+++ src/routes/khoavien.js
@@ -5,25 +5,25 @@ var bodyParser = require('body-parser')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 router.use(bodyParser.urlencoded({ extended: false }))
 
-module.exports = router;
+module.exports = router
 
-router.get('/danhsach', function(req, res, next) {
+router.get('/danhsach', (req, res, next) => {
     (async() => {
         const client = await pool.connect()
         try {
             const result = await client.query('SELECT * FROM khoavien')
-            res.send(result.rows)
+            res.render('./admin/layout/index')
         } finally {
             client.release()
         }
     })().catch(e => console.log(e.stack))
 });
 
-router.get('/them', function(req, res, next) {
+router.get('/them', (req, res, next) => {
     res.render("./khoavien/them")
 });
 
-router.post('/them', function(req, res, next) {
+router.post('/them', (req, res, next) => {
     const tenkhoavien = req.body.tenkhoavien;
     console.log(req.body);
     (async() => {
