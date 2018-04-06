c4239a98a16a805dbee508edee8c9dd2e6a1472f
huyenngau <huyenn.230197@gmail.com>
2018-04-06 14:55:42 +0700

    ver1 loaidoan

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const users = require('./users')
 const khoavien = require('./khoavien')
 const admin = require('./admin')
+const loaidoan = require('./loaidoan')
 
 module.exports = (app) => {
   app.use('/users', users)
   app.use('/khoavien', khoavien)
   app.use('/admin', admin)
+  app.use('/loaidoan', loaidoan)
 }
