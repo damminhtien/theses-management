ba557bfaa7757d930718c6560118e7d0dd864cd7
HuongMeo <quynhhuong09111997@gmail.com>
2018-04-06 10:02:39 +0700

    Hoan thanh Trang Thai

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const users = require('./users')
 const khoavien = require('./khoavien')
 const admin = require('./admin')
+const trangthai = require('./trangthai')
 
 module.exports = (app) => {
   app.use('/users', users)
   app.use('/khoavien', khoavien)
   app.use('/admin', admin)
+  app.use('/trangthai', trangthai)
 }
