6a1061645a16b28be9fc3d2c398fd948f7bbbbb4
damminhtien <damminhtienchl@gmail.com>
2018-04-08 01:27:38 +0700

    add timeline

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
-const users = require('./users')
 const khoavien = require('./khoavien')
 const admin = require('./admin')
 const loaidoan = require('./loaidoan')
 const trangthai = require('./trangthai')
+const giangvien = require('./giangvien')
 
 module.exports = (app) => {
-  app.use('/users', users)
   app.use('/khoavien', khoavien)
   app.use('/admin', admin)
   app.use('/loaidoan', loaidoan)
   app.use('/trangthai', trangthai)
+  app.use('/giangvien', giangvien)
 }
